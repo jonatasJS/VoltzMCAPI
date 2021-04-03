7e4262d354af3122cce3a3daee3c9b34840feead
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-04-03 08:10:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const cors = require('cors');
 app.use(cors());
 
 function go() {
-  console.log('Go');
   app.get('/', async (req, res) => {
-    console.log('Foi!2');
     await util.status('jogar.voltzmc.com.br')
     .then(async (resS) => {
-      console.log(resS.onlinePlayers);
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
       res.json(resS);
-      console.log('Foi!3');
     })
     .catch((err) => {
       console.error(err);
@@ -26,5 +24,4 @@ setInterval(go, 10000);
 
 app.listen(8080, () => {
   console.clear();
-  console.log('Foi!1');
 });
