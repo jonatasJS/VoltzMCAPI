4d03f0838eea5c34ae5d035844d7f5165167d385
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-04-10 22:06:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 async function go() {
+	await app.get('/:server', async (req, res) => {
+		const server = req.params.server;
+
+		await util.status(server)
+		.then(async resS => {
+			res.json(resS)
+		})
+		.catch(err => console.log(err));
+	});
+
   await app.get('/', async (req, res) => {
     await util.status('jogar.voltzmc.com.br')
     .then(async (resS) => {
